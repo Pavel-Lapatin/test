fe592a411b5d7d1b2aff0c7811e51e3cf408089a
Pavel Lapatsin <p.lapatsin@godeltech.com>
2021-02-23 08:29:49 +0000

    Add Nancy lib

---
+++ src/VulnerableLibraries/Modules/FooModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace VulnerableLibraries.Modules
+{
+    public class FooModule : NancyModule
+    {
+        public FooModule()
+        {
+            Get["Bar"] = parameters => { return "You have reached the /bar route"; };
+        }
+    }
+}
---
+++ src/VulnerableLibraries/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace VulnerableLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:1234")))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:1234");
+                Console.ReadLine();
+            }
+        }
+    }
+}
